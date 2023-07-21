22479bb39f6412b8ac6ebb6dcab4eba22dee1347
Swagnik02 <swagnik1234@gmail.com>
2023-07-21 14:10:52 +0530

    added basic register page

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
 
 class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+  const RegisterView({Key? key}) : super(key: key);
 
   @override
   State<RegisterView> createState() => _RegisterViewState();
 }
 
 class _RegisterViewState extends State<RegisterView> {
-  @override
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-     Widget build(BuildContext context) {
-    return const Scaffold(
+  // Function to handle the registration logic
+  void _registerUser() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    // Implement your registration logic here
+    // For example, you can use FirebaseAuth or your custom backend API
+    // to register the user with the provided email and password.
+
+    // After successful registration, you can navigate to the login page.
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your email',
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter your password',
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: _registerUser,
+              child: const Text('Register'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
