d1e61bfca47c15260c65fb7280df025f3254224f
Swagnik02 <swagnik1234@gmail.com>
2023-07-22 13:36:18 +0530

    added ui colours for auth page

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 class ColorConstants {
   // static const Color primaryColor = Color.fromARGB(a, r, g, b);
-  static const Color accentColor = Color.fromARGB(148, 255, 155, 4);
-  static const Color btnEnabled = Color.fromARGB(187, 255, 154, 4);
-  static const Color btnOnPressed = Color.fromARGB(147, 194, 167, 128);
-  static const Color textFieldBg = Color.fromARGB(32, 210, 139, 33);
+
+  //auth page
+  static const Color authBackground = Color.fromARGB(255, 35, 45, 54);
+  static const Color authTextFieldBg = Color.fromARGB(25, 35, 45, 54);
+  static const Color authButtonActive = Color.fromARGB(255, 30, 136, 229);
+  static const Color authText = Color.fromARGB(255, 255, 255, 255);
+
+  //Profile page
+
+  //Home Page
+
   // Add more color constants as needed for your project
 }
