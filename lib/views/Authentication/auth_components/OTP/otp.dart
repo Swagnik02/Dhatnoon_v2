02430bab8997734960ca2b54fa6c7f9c37bf384c
Swagnik02 <swagnik1234@gmail.com>
2023-08-06 04:30:44 +0530

    pinput validator not working

--- lib/views/Authentication/auth_components/OTP/otp.dart
+++ lib/views/Authentication/auth_components/OTP/otp.dart
@@ -9,6 +9,74 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pinput/pinput.dart';
 import 'dart:developer' as devtools show log;
 
+class OtpDailogue {
+  void showTextFieldPopup(BuildContext context) {
+    var code = "";
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Enter Text'),
+          content: Column(
+            children: [
+              Pinput(
+                length: 6,
+                defaultPinTheme: defaultPinTheme,
+                focusedPinTheme: focusedPinTheme,
+                submittedPinTheme: submittedPinTheme,
+                onChanged: (value) {
+                  code = value;
+                },
+                // validator: (pin) {
+                //   if (pin == '2224') return null;
+                //   return 'Pin is incorrect';
+                // },
+                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                showCursor: true,
+                onCompleted: (pin) => devtools.log(pin),
+              ),
+              TextButton(
+                onPressed: () {
+                  Fluttertoast.showToast(
+                    msg: "Resend",
+                  );
+                },
+                child: const Text('Resend'),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  devtools.log(SignUpView.verify);
+                  devtools.log(code);
+                  // Create a PhoneAuthCredential with the code
+                  PhoneAuthCredential credential = PhoneAuthProvider.credential(
+                      verificationId: SignUpView.verify, smsCode: code);
+
+                  // Sign the user in (or link) with the credential
+                  await auth.signInWithCredential(credential);
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    mainRoute,
+                    (route) => false,
+                  );
+                } catch (e) {
+                  devtools.log('$e');
+                }
+              },
+              child: const Text('Verify'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+
 // class OTPScreen extends StatefulWidget {
 //   const OTPScreen({super.key});
 //   @override
@@ -77,67 +145,3 @@ import 'dart:developer' as devtools show log;
 //     ));
 //   }
 // }
-
-class OtpDailogue {
-  void showTextFieldPopup(BuildContext context) {
-    var code = "";
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Enter Text'),
-          content: Column(
-            children: [
-              Pinput(
-                length: 6,
-                defaultPinTheme: defaultPinTheme,
-                focusedPinTheme: focusedPinTheme,
-                submittedPinTheme: submittedPinTheme,
-                onChanged: (value) {
-                  code = value;
-                },
-                validator: (s) {
-                  return s == '2222' ? null : 'Pin is incorrect';
-                },
-                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
-                showCursor: true,
-                onCompleted: (pin) => devtools.log(pin),
-              ),
-              TextButton(
-                onPressed: () {
-                  Fluttertoast.showToast(
-                    msg: "Resend",
-                  );
-                },
-                child: const Text('Resend'),
-              ),
-            ],
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  // Create a PhoneAuthCredential with the code
-                  PhoneAuthCredential credential = PhoneAuthProvider.credential(
-                      verificationId: SignUpView.verify, smsCode: code);
-
-                  // Sign the user in (or link) with the credential
-                  await auth.signInWithCredential(credential);
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    mainRoute,
-                    (route) => false,
-                  );
-                } catch (e) {
-                  devtools.log('$e');
-                }
-              },
-              child: const Text('Verify'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
