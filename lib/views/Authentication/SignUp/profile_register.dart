2a4aee0b38621a53aca5532b77fd293e5087eb6d
Swagnik02 <swagnik1234@gmail.com>
2023-08-06 22:52:01 +0530

    added username in RegisterProfile page

--- lib/views/Authentication/SignUp/profile_register.dart
+++ lib/views/Authentication/SignUp/profile_register.dart
@@ -1,64 +1,88 @@
 import 'package:dhatnoon_v2/constants/color_constants.dart';
-import 'package:dhatnoon_v2/views/Authentication/auth_components/CustomTemplates/auth_btn_ui.dart';
 import 'package:dhatnoon_v2/views/Authentication/auth_components/auth_text_fields.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ProfRegister extends StatelessWidget {
   const ProfRegister({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController _userName = TextEditingController();
     TextEditingController _firstName = TextEditingController();
     TextEditingController _lastName = TextEditingController();
+    // final auth = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       backgroundColor: ColorConstants.authBackground,
       appBar: AppBar(
-        elevation: 0, // Set elevation to 0 to remove shadow from the app bar
+        elevation: 0,
         backgroundColor: Colors.transparent,
         leading: IconButton(
           color: ColorConstants.authBackBtn,
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
           onPressed: () => Navigator.pop(context),
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(16.0, 20, 16, 40),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Center(
-              child: Text(
-                "What's your name ?",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                  child: Text(
+                    "What's your name ?",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+                const SizedBox(height: 27),
+                const Text(
+                  "FIRST NAME",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                AuthTextField(controller: _firstName),
+                const SizedBox(height: 27),
+                const Text(
+                  "LAST NAME",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                AuthTextField(controller: _lastName),
+                const SizedBox(height: 27),
+                const Text(
+                  "USERNAME",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                AuthTextField(controller: _userName),
+              ],
             ),
-            const SizedBox(height: 27),
-            //FIRST NAME
-            const Text(
-              "FIRST NAME",
-              style: TextStyle(color: Colors.white, fontSize: 15),
-            ),
-            AuthTextField(controller: _firstName),
-            //LAST NAME
-            const SizedBox(height: 27),
-            const Text(
-              "LAST NAME",
-              style: TextStyle(color: Colors.white, fontSize: 15),
-            ),
-            const SizedBox(height: 5),
-            AuthTextField(controller: _lastName),
-
-            const SizedBox(height: 20),
-            AuthBtnUI(
-              onPressed: () {
-                // Add your signup button action here
-              },
-              text: 'Sign Up & Accept',
-            ),
-            const SizedBox(width: 10),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Fluttertoast.showToast(msg: "Your message here");
+        },
+        child: const Icon(Icons.arrow_forward_ios_sharp),
       ),
     );
   }
 }
+
+
+
+
+            // Column(
+
+            //   children: [
+            //     AuthBtnUI(
+            //       onPressed: () {
+            //         // Add your signup button action here
+            //       },
+            //       text: 'Sign Up & Accept',
+            //     ),
+            //   ],
+            // ),
